23afef8a595c235aee441cc709579d622b1a64e3
fcyprowski <f.cyprowski@gmail.com>
2022-05-29 13:54:50 +0200

    Update server.R

--- shiny_diamonds/server.R
+++ shiny_diamonds/server.R
@@ -1,15 +1,25 @@
 library("shiny")
 library("tidyverse")
+library("plotly")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  output$diamonds_plot1 <- renderPlot({
+  diamonds_filtered <- reactive({
     diamonds %>%
       filter(
         cut == input$cut,
         carat <= input$carat
-      ) %>%
+      )
+  })
+  output$diamonds_plot1 <- renderPlot({
+    diamonds_filtered() %>%
       ggplot(aes(x = carat, y = price)) +
       geom_point()
   })
+  output$depth_plot <- renderPlotly({
+    diamonds_filtered() %>%
+      plot_ly(x = ~depth, y = ~price) %>%
+      add_markers(color = ~clarity, size = ~carat, alpha = .2)
+  })
 })
+
